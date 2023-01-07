90457ebd98509632b53c04c0f88292b52dee2713
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-07 18:39:19 -0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Orlando Moraes Martins on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
