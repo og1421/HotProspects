473e66fb6a8d531e1091a98768dfe26f7c801712
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-07 19:09:25 -0300

    Reading custom values from the environment

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+@MainActor class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+    
+}
+
+struct EditView: View{
+    @EnvironmentObject var user: User
+    
+    var body: some View{
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View{
+        Text(user.name)
+    }
+}
+
 struct ContentView: View {
+    @StateObject var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            EditView()
+            DisplayView()
         }
-        .padding()
+        .environmentObject(user)
     }
 }
 
